d5d6163e66f6a117dbfcc1d57b20c85058fbfad5
Nikhil11100 <nikhilthakare88@gmail.com>
2024-05-14 17:11:05 +0530

    Delete select2.py

--- select2.py
+++
@@ -1,1 +0,0 @@
- SELECT count(*) FROM Transaction WHERE name LIKE '%getLoadv2/loadv2%' AND (appName LIKE '%nonprod-test%' OR appName LIKE '%dev') FACET tags.Customer, tags.Environment SINCE 30 MINUTES AGO TIMESERIES 20 SECONDS LIMIT MAX
